a3777a0512f478f378ecc7fca88cae5d7c396b42
Celso Nery <celso.nery@gmail.com>
2024-10-11 20:16:14 -0300

    feat: Adiciona carousel

--- src/components/CarouselComponent.vue
+++ src/components/CarouselComponent.vue
@@ -1,51 +1,20 @@
 <template>
-  <div id="default-carousel" class="w-full" data-carousel="slide">
-    <!-- Carousel wrapper -->
-    <div class="relative overflow-hidden mb-2 -mt-2">
-      <!-- Item 1 -->
-      <div class="duration-700 ease-in-out" data-carousel-item>
-        <img src="@/assets/images/slider/slide-01.jpg" class="block w-full" alt="...">
-      </div>
-<!--      &lt;!&ndash; Item 2 &ndash;&gt;-->
-<!--      <div class="hidden duration-700 ease-in-out" data-carousel-item>-->
-<!--        <img src="@/assets/images/slider/slide-02.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">-->
-<!--      </div>-->
-<!--      &lt;!&ndash; Item 3 &ndash;&gt;-->
-<!--      <div class="hidden duration-700 ease-in-out" data-carousel-item>-->
-<!--        <img src="@/assets/images/slider/slide-03.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">-->
-<!--      </div>-->
-<!--      &lt;!&ndash; Item 4 &ndash;&gt;-->
-<!--      <div class="hidden duration-700 ease-in-out" data-carousel-item>-->
-<!--        <img src="@/assets/images/slider/slide-04.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">-->
-<!--      </div>-->
-    </div>
-    <!-- Slider indicators -->
-    <div class="absolute z-30 flex -translate-x-1/2 bottom-3 left-1/2 space-x-3 rtl:space-x-reverse">
-      <button type="button" class="w-2 h-2 md:w-3 md:h-3 rounded-full bg-white" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-<!--      <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>-->
-<!--      <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>-->
-<!--      <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>-->
-    </div>
-    <!-- Slider controls -->
-<!--    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>-->
-<!--        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">-->
-<!--            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">-->
-<!--                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>-->
-<!--            </svg>-->
-<!--            <span class="sr-only">Previous</span>-->
-<!--        </span>-->
-<!--    </button>-->
-<!--    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>-->
-<!--        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">-->
-<!--            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">-->
-<!--                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>-->
-<!--            </svg>-->
-<!--            <span class="sr-only">Next</span>-->
-<!--        </span>-->
-<!--    </button>-->
-  </div>
+  <carousel :autoplay="3000" :wrap-around="true" :items-to-show="1">
+    <slide v-for="slide in items" :key="slide.id">
+      <img :src="slide.src" alt="slide">
+    </slide>
+  </carousel>
 </template>
 
 <script setup>
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide } from 'vue3-carousel'
+
+const items = [
+  {id: 0, src: 'images/slider/slide-01.jpg' },
+  {id: 1, src: 'images/slider/slide-02.jpg' },
+  {id: 2, src: 'images/slider/slide-03.jpg' },
+  {id: 3, src: 'images/slider/slide-04.jpg' },
+]
 
 </script>
